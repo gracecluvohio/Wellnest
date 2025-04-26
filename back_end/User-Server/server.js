94d3b928590280aaab685adff508b2fff25b2e8d
Yunyi Gao <gao622@purdue.edu>
2025-04-26 12:35:40 -0400

    Back end structure setup

---
+++ back_end/User-Server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/login', (req, res) => {
+    res.send('Hello from server');
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening on port ${port}`);
+});
--- back_end/start-services.sh
+++ back_end/start-services.sh
@@ -1,3 +1,4 @@
+docker compose down --volumes
 docker compose build
 docker compose up -d
 docker compose logs -f wellnest-nginx
