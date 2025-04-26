ce2b1876e823c30bc9e0d2937a00a5ddfb7ff792
Grace Chia <gracecluvohio@gmail.com>
2025-04-26 00:22:28 -0700

    Add wireframe for dashboard

--- front_end/app/(tabs)/_layout.tsx
+++ front_end/app/(tabs)/_layout.tsx
@@ -8,46 +8,52 @@ export default function TabLayout() {
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor: '#ffd33d', 
-                headerStyle: {
-                    backgroundColor: '#25292e', 
-                }, 
-                headerShadowVisible: false, 
-                headerTintColor: '#fff',
-                tabBarStyle: {
-                    backgroundColor: '#25292e',
-                },
+            tabBarActiveTintColor: '#ffd33d',
+            headerStyle: {
+                backgroundColor: '#25292e',
+            },
+            headerShadowVisible: false,
+            headerTintColor: '#fff',
+            tabBarStyle: {
+                backgroundColor: '#25292e',
+            },
             }}
         >
-            <Tabs.Screen 
-                name="index" 
-                options={{ 
-                    headerTitleAlign: HEADER_ALIGNMENT,
-                    title: 'Home',
-                    tabBarIcon: ({ color, focused }) => (
-                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={ICON_SIZE} />
-                    ),
-                }} 
+            <Tabs.Screen
+            name="index"
+            options={{
+                headerTitleAlign: HEADER_ALIGNMENT,
+                title: 'Home',
+                tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={ICON_SIZE} />
+                ),
+                headerRight: () => (
+                    <>
+                        <Ionicons name="settings" size={ICON_SIZE} color="#fff" style={{ marginRight: 15 }} />
+                        <Ionicons name="help" size={ICON_SIZE} color="#fff" style={{ marginRight: 15 }} />
+                    </>
+                ),
+            }}
             />
-            <Tabs.Screen 
-                name="chat" 
-                options={{ 
-                    headerTitleAlign: HEADER_ALIGNMENT,
-                    title: 'Chat',
-                    tabBarIcon: ({ color, focused }) => (
-                        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={ICON_SIZE}/>
-                    ),
-                }} 
+            <Tabs.Screen
+            name="chat"
+            options={{
+                headerTitleAlign: HEADER_ALIGNMENT,
+                title: 'Chat',
+                tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={ICON_SIZE} />
+                ),
+            }}
             />
-            <Tabs.Screen 
-                name="add" 
-                options={{ 
-                    headerTitleAlign: HEADER_ALIGNMENT,
-                    title: 'Add',
-                    tabBarIcon: ({ color, focused }) => (
-                        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={ICON_SIZE}/>
-                    ),
-                }} 
+            <Tabs.Screen
+            name="add"
+            options={{
+                headerTitleAlign: HEADER_ALIGNMENT,
+                title: 'Add',
+                tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={ICON_SIZE} />
+                ),
+            }}
             />
         </Tabs>
     );
--- front_end/app/(tabs)/index.tsx
+++ front_end/app/(tabs)/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
+import Div from "@/components/div";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.divContainer}>
+        <Div  />
+        <Div  />
+        <Div  />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  }, 
+  divContainer: {
+    flex: 1, 
+    alignItems: 'center', 
+  }
+})
