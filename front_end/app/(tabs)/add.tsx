af58f249686bd6f46173b76cad3100fa5df489e8
pogi7 <aaronlevitt7@gmail.com>
2025-04-26 09:51:02 -0700

    make wireframe/layout for add page
    Signed-off-by: pogi7 <aaronlevitt7@gmail.com>

--- front_end/app/(tabs)/add.tsx
+++ front_end/app/(tabs)/add.tsx
@@ -1,21 +1,45 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+const DEFAULT_ICON_SIZE = 24;
+const PLUS_ICON_SIZE = 96;
+const SWIPE_LEFT = false;
+const SWIPE_RIGHT = false;
 
 export default function Add() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Add screen</Text>
-        </View>
-    );
+  return (
+    <>
+      <View style={styles.add_screen}>
+        <Ionicons size={PLUS_ICON_SIZE} name="cloud-upload" color="white" />
+      </View>
+      {/* TODO: Add logic to handle icons changing on tap */}
+      {/* TODO: handle form input  */}
+      <View style={styles.add_options}>
+        <Text style={styles.manual_text}>Or Manual Input</Text>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        backgroundColor: '#25292e', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-    }, 
-    text: {
-        color: '#fff', 
-    },
+  add_screen: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  add_options: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  manual_text: {
+    fontSize: 24,
+    color: "#fff",
+  },
+  text: {
+    color: "#fff",
+  },
 });
