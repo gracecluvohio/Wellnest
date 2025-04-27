8f7f9bb1be4cefbe42be779a3ea94866c1cfb2c8
Grace Chia <gracecluvohio@gmail.com>
2025-04-27 08:41:00 -0700

    Stylize landing

--- front_end/app/landing.tsx
+++ front_end/app/landing.tsx
@@ -1,20 +1,26 @@
 import { View, StyleSheet, Text, Pressable } from "react-native";
 import { useNavigation } from "expo-router";
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 
 export default function Landing() {
 
     const navigation = useNavigation();
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Hello, welcome to Wellnest. This is your personal healthcare provider. </Text>
-            <Pressable onPressOut={()=> {
-                // @ts-expect-error
-                navigation.navigate("(tabs)");
-            }}>
-                <Text style={[styles.text, { marginTop: 16, fontSize: 16, color: '#88D498', marginLeft: 200}]}>Enter</Text>
-            </Pressable>
-        </View>
+        <Pressable style={styles.container} onPressOut={()=> {
+            // @ts-expect-error
+            navigation.navigate("(tabs)");
+        }}>
+        <Animated.View 
+            style={styles.container}
+            entering={FadeIn.duration(1000)}
+            exiting={FadeOut.duration(400)}>
+            
+                <Text style={styles.text}>Hello, welcome to Wellnest.</Text>
+            
+        </Animated.View>
+        
+        </Pressable>
     );
 }
 
@@ -26,7 +32,7 @@ const styles = StyleSheet.create({
     }, 
     text: {
         marginHorizontal: 48, 
-        marginTop: 320, 
+        marginTop: 360, 
         fontSize: 24, 
         color: '#222',
     }
